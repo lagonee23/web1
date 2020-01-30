389bed6fd1c843cd70894d98754f80034b83a22a
lagonee23 <59104524+lagonee23@users.noreply.github.com>
2020-01-30 20:23:30 +0900

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,22 @@
+var Everything = {
+    movli : ['겨울왕국', '겨울왕국2', '알라딘'],
+    list : function() {
+        var i=0;
+        while(i<this.movli.length) {
+            document.write('<li><a href="page_'+i+'.html">'+this.movli[i]+'</a></li>');
+            i=i+1;
+        }
+    },
+    backColorOnOff : function(self, color1, color2) {
+        var target=document.querySelector('#article');
+        if(self.value==='ON'){
+            target.style.backgroundColor=color1;
+            target.style.color=color2;
+            self.value='OFF';
+        }else{
+            target.style.backgroundColor='white';
+            target.style.color='black'
+            self.value='ON';
+        }
+    }
+}
